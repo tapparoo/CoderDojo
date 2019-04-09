0210bf7e9774aa100896d4be66c8c515e5fe14a4
Anna Jimenez <annajimenez146@gmail.com>
2019-04-09 16:27:12 -0600

    JPA and Boot projects, link projects

---
+++ CoderDojo/src/main/java/com/skilldistillery/coderdojo/CoderDojoApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.coderdojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoderDojoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoderDojoApplication.class, args);
+	}
+
+}
---
+++ CoderDojo/src/test/java/com/skilldistillery/coderdojo/CoderDojoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.skilldistillery.coderdojo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CoderDojoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
