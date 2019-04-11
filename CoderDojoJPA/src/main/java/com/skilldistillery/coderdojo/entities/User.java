7363eb3f72207a495efdd59f678739b4b3e92209
Adam Tappy <adamtappy@Brucebs-MacBook-Pro-2.local>
2019-04-11 10:20:58 -0600

    fixed User-role many-to-many

--- CoderDojoJPA/src/main/java/com/skilldistillery/coderdojo/entities/User.java
+++ CoderDojoJPA/src/main/java/com/skilldistillery/coderdojo/entities/User.java
@@ -1,11 +1,14 @@
 package com.skilldistillery.coderdojo.entities;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
@@ -13,62 +16,80 @@ import javax.persistence.Transient;
 @Entity
 @Table(name = "user")
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    private String username;
+	private String username;
 
-    private String password;
-    
-    private boolean enabled;
+	private String password;
 
+	private boolean enabled;
 
 	@Transient
-    private String passwordConfirm;
+	private String passwordConfirm;
 
-    @ManyToMany
-    private Set<Role> roles;
+	@ManyToMany
+	@JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles;
 
-    public Long getId() {
-        return id;
-    }
+	public void addRole(Role role) {
+		if (role == null)
+			return;
+		if (roles == null)
+			roles = new HashSet<>();
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+		roles.add(role);
+		role.getUsers().add(this);
+	}
 
-    public String getUsername() {
-        return username;
-    }
+	public void removeRole(Role role) {
+		if (role == null)
+			return;
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+		roles.remove(role);
+		role.getUsers().remove(this);
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getPasswordConfirm() {
-        return passwordConfirm;
-    }
+	public String getUsername() {
+		return username;
+	}
 
-    public void setPasswordConfirm(String passwordConfirm) {
-        this.passwordConfirm = passwordConfirm;
-    }
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
-    public Set<Role> getRoles() {
-        return roles;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
-    }
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPasswordConfirm() {
+		return passwordConfirm;
+	}
+
+	public void setPasswordConfirm(String passwordConfirm) {
+		this.passwordConfirm = passwordConfirm;
+	}
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
 
 	public boolean isEnabled() {
 		return enabled;
@@ -77,4 +98,36 @@ public class User {
 	public void setEnabled(boolean enabled) {
 		this.enabled = enabled;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", password=" + password + ", enabled=" + enabled
+				+ ", passwordConfirm=" + passwordConfirm + ", roles=" + roles + "]";
+	}
+
 }
