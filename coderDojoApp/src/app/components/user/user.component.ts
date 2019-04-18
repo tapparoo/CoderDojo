212a7ee65c303ac1416af068f0bdff240c49e173
Anna Jimenez <annajimenez146@gmail.com>
2019-04-17 18:06:28 -0600

    wip user

--- coderDojoApp/src/app/components/user/user.component.ts
+++ coderDojoApp/src/app/components/user/user.component.ts
@@ -1,15 +1,15 @@
-import { UserDetail } from './../../models/user-detail';
-import { UserService } from 'src/app/services/user.service';
-import { AuthService } from './../../services/auth.service';
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { NgForm } from '@angular/forms';
-import { User } from 'src/app/models/user';
+import { UserDetail } from "./../../models/user-detail";
+import { UserService } from "src/app/services/user.service";
+import { AuthService } from "./../../services/auth.service";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { NgForm } from "@angular/forms";
+import { User } from "src/app/models/user";
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  selector: "app-user",
+  templateUrl: "./user.component.html",
+  styleUrls: ["./user.component.css"]
 })
 export class UserComponent implements OnInit {
   user = null;
@@ -17,69 +17,54 @@ export class UserComponent implements OnInit {
   achievements = [];
   children = [];
   newChild = false;
-  childrenAchivments =[];
-  navigateToUserProfile(username, parentname){
-    this.router.navigateByUrl('user/'+username+ '/profile/parent');
+  childrenAchivments = [];
+  navigateToUserProfile(username, parentname) {
+    this.router.navigateByUrl("user/" + username + "/profile/parent");
   }
 
-  addChild(form: NgForm){
-    const user = new User(form.value.nickname, 'password', true);
+  addChild(form: NgForm) {
+    const user = new User(form.value.nickname, "password", true);
     // Add User
-    this.auth.registerChild(user).subscribe(
-      data => {
-        this.userService.addChild(this.user.user.username, data).subscribe(
-          child => {
-            // Add UserDetail
-            child.nickname = form.value.nickname;
-            child.firstName = form.value.firstName;
-            child.lastName = form.value.lastName;
-            child.dob = form.value.dob;
-            this.userService.updateUserDetail(child).subscribe(
-              deets => {
-                console.log(deets);
-                // Reload parent's children array
-                this.reloadChildren();
-              }
-            );
-          }
-          );
-      }
-    );
+    this.auth.registerChild(user).subscribe(data => {
+      this.userService
+        .addChild(this.user.user.username, data)
+        .subscribe(child => {
+          // Add UserDetail
+          child.nickname = form.value.nickname;
+          child.firstName = form.value.firstName;
+          child.lastName = form.value.lastName;
+          child.dob = form.value.dob;
+          this.userService.updateUserDetail(child).subscribe(deets => {
+            console.log(deets);
+            // Reload parent's children array
+            this.reloadChildren();
+          });
+        });
+    });
   }
 
   // Reload parent's children array
   reloadChildren() {
-    console.log(this.user);
-    this.userService.getChildren(this.user.user.username).subscribe(
-      data => {
-        this.children = data
-        console.log( this.children);
-        for (const key of this.children) {
-          // console.log(key)
-          this.userService.getUserAchievements(key.user.username).subscribe(
-            achieves => {
-
-              // console.log(this.user);
+    this.userService.getChildren(this.user.user.username).subscribe(data => {
+      this.user.children = data;
+      for (const key of this.user.children) {
+        this.userService.getUserAchievements(key.user.username).subscribe(
+          achieves => {
+            console.log(achieves);
+            if (achieves) {
               key.achievments = achieves;
-              // console.log( key);
-
-                 },
-            err => console.error('Observer got an error: ' + err)
-          );
-          //key.achievments = achieves;
-
-          console.log(this.children);
-        }
+            }
+          },
+          err => console.error("Observer got an error: " + err)
+        );
       }
-    );
-    // console.log(this.user);
-
+    });
   }
 
   reloadMeetings() {
-    this.userService.getUserMeetings(this.user.user.username).subscribe(
-      data => this.user.meetings = data
-    );
+    this.userService
+      .getUserMeetings(this.user.user.username)
+      .subscribe(data => (this.user.meetings = data));
   }
 
   constructor(
@@ -87,33 +72,35 @@ export class UserComponent implements OnInit {
     private router: Router,
     private currentRoute: ActivatedRoute,
     private userService: UserService
-    ) { }
+  ) {}
 
   ngOnInit() {
     if (this.auth.checkLogin()) {
-      if (this.currentRoute.snapshot.paramMap.get('username')) {
-        this.userService.getUser(this.currentRoute.snapshot.paramMap.get('username')).subscribe(
-          data => {
-            console.log(data)
-            this.user = data;
-            this.userService.getUserAchievements(this.user.user.username).subscribe(
-              achieves => {
-                this.user.achievements = achieves;
-                this.achievements = achieves;
-              },
-              err => console.error('Observer got an error: ' + err)
-            );
-            this.reloadMeetings();
-            this.reloadChildren();
-
-          },
-          err => {
-            this.router.navigateByUrl('not-found');
-            console.error('Observer got an error: ' + err);
-          }
-        );
+      if (this.currentRoute.snapshot.paramMap.get("username")) {
+        this.userService
+          .getUser(this.currentRoute.snapshot.paramMap.get("username"))
+          .subscribe(
+            data => {
+              console.log(data);
+              this.user = data;
+              this.userService
+                .getUserAchievements(this.user.user.username)
+                .subscribe(
+                  achieves => {
+                    this.user.achievements = achieves;
+                    this.achievements = achieves;
+                  },
+                  err => console.error("Observer got an error: " + err)
+                );
+              this.reloadMeetings();
+              this.reloadChildren();
+            },
+            err => {
+              this.router.navigateByUrl("not-found");
+              console.error("Observer got an error: " + err);
+            }
+          );
       }
     }
-
   }
 }
